cb573d561ade0a4823d441acc6aacedb7edda347
Fabos <fabos180@gmail.com>
2023-07-24 17:58:29 -0500

    Intial Project

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Bienvenidos a nuestra Prueba de Concepto de DevSecOps con React y GitHub Actions!
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visita nuestro repositorio en GitHub
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
