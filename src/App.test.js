70654c5b4147594876c57224a79735568c2d745e
Fabos <fabos180@gmail.com>
2023-07-24 22:45:05 -0500

    ajustes test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('verifies link text in App component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Visita nuestro repositorio en GitHub/i);
   expect(linkElement).toBeInTheDocument();
 });
